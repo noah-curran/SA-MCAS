528271171d86f383eaf1922a2d7e92b995f04aa2
Noah Curran <swimnoah113@gmail.com>
2022-05-09 15:41:45 -0400

    add a list of types of detectors we can use

---
+++ src/anomaly-detection/types/DetectorType.m
@@ -0,0 +1,12 @@
+classdef DetectorType
+    %DETECTORTYPE Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    enumeration
+      ClosedForm ("Closed Form")
+      LinearRegression ("Linear Regression")
+      LogisticRegression ("Logistic Regression")
+      PolynomialRegression ("Polynomial Regression")
+    end
+end
+
