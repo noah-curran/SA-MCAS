cf9f1ae261246b9659834b8db04727c8da10a51e
ThomasKennings <kennings@umich.edu>
2023-04-02 12:48:11 -0400

    'finished' reacting pilot

--- simulation/run_simulation_manual.m
+++ simulation/run_simulation_manual.m
@@ -10,4 +10,4 @@ initialize_sim_config;
 store_anomaly_params(append('anomalies/EmptyInjection.json'));
 set_script_parameters([zeros(1, 21), 50, 300, 100, 3]);
 filename = append("../data-collection/simulation-export/manual_output.csv");
-do_sim("takeoff_stall_pilot_reaction_delay", "sa_mcas", 300, evalin("base", "sortedParams"), 1, 0, 0, 0, filename);
+do_sim("takeoff_stall_pilot_reaction_delay", "sa_mcas", 600, evalin("base", "sortedParams"), 1, 0, 0, 0, filename);
