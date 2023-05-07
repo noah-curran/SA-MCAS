cb32d709c4300606e3790ddb81d8d8e896843e3c
ThomasKennings <kennings@umich.edu>
2023-05-07 17:39:45 -0400

    added energy and other code

--- simulation/set_mcas_parameters.m
+++ simulation/set_mcas_parameters.m
@@ -6,5 +6,12 @@ end % function
 % 1. MCAS cooldown time
 % 2. MCAS trim amount (deg)
 % 3. MCAS trim rate (deg/s)
+% 4. Trim wheel rotations per degree
+% 5. Trim wheel rotations per second (as performed by pilot)
+% 6. Initial pilot energy
+% 7. Energy input rate
+% 8. Background energy burn rate
+% 9. Trimming energy burn rate
 
-% "defaults" would be cooldown=11, trim_amount=2.5, trim_rate=0.27
+% "defaults" would be cooldown=11, trim_amount=2.5, trim_rate=0.27,
+% rotations_per_degree=18, rotations_per_second=7.2
